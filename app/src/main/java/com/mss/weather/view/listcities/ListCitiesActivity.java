7505c081e7bc71592c07b7176586f63f722ddc68
Maklakov Sergey <maklakov.sergey@hotmail.com>
2018-12-03 10:21:35 +0500

    добавлена картинка на кнопку добавления города

--- app/src/main/java/com/mss/weather/view/listcities/ListCitiesActivity.java
+++ app/src/main/java/com/mss/weather/view/listcities/ListCitiesActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
@@ -25,7 +25,7 @@ public class ListCitiesActivity extends MvpAppCompatActivity implements ListCiti
     @BindView(R.id.rvCitiesList)
     RecyclerView rvCitiesList;
     @BindView(R.id.btnAddCity)
-    Button btnAddCity;
+    ImageButton btnAddCity;
 
     private Unbinder binder;
 
